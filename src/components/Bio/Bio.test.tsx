bfb96b72e620c50e756315f0713e03cd1e45b401
mikevilla <mikevilla@gmail.com>
2023-02-19 07:23:25 -0800

    update bio test

--- src/components/Bio/Bio.test.tsx
+++ src/components/Bio/Bio.test.tsx
@@ -1,18 +1,44 @@
 import React from 'react';
 import { render, screen } from '@testing-library/react';
-import { Provider } from 'react-redux';
-import { store } from '../../app/store';
-import App from '../../App';
+import Bio from '../Bio/Bio'
 
-test('Bio Component', () => {
+describe('Bio Section', () => {
 
-    render(
-        <Provider store={store}>
-            <App />
-        </Provider>
-    );
+    test('renders Bio Component with data', () => {
 
-    const searchTitle = screen.getByText(/Search for Pokemon/i);
-    expect(searchTitle).toBeInTheDocument()
+        const bioData = {
+            abilities: [],
+            capture_rate: '45',
+            egg_groups: [
+                {name: 'monster', url: 'https://pokeapi.co/api/v2/egg-group/1/'},
+                {name: 'plant', url: 'https://pokeapi.co/api/v2/egg-group/7/'}],
+            gender_rate: '1',
+            growth_rate: {name: 'medium-slow', url: 'https://pokeapi.co/api/v2/growth-rate/4/'},
+            habitat: {name: 'grassland', url: 'https://pokeapi.co/api/v2/pokemon-habitat/3/'},
+            height: '10',
+            weight: '130'
+        }
 
-});
+        const { abilities, capture_rate, egg_groups, gender_rate, growth_rate, habitat, height, weight } = bioData;
+
+        render(
+            <Bio
+                abilities={abilities}
+                capture_rate={capture_rate}
+                egg_groups={egg_groups}
+                gender_rate={gender_rate}
+                height={height}
+                weight={weight}
+                habitat={habitat}
+                growth_rate={growth_rate}
+            />    );
+
+        const searchHabitat = screen.getByText(/grassland/i);
+        const searchCaptureRate = screen.getByText(/18 %/i);
+        expect(searchHabitat).toBeInTheDocument();
+        expect(searchCaptureRate).toBeInTheDocument();
+
+    });
+
+})
+
