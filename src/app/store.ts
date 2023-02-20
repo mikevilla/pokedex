fe1e3cd1e08fb5479bbaaf67b3043ae48e22b561
mikevilla <mikevilla@gmail.com>
2023-02-19 20:02:50 -0800

    remove sample code

--- src/app/store.ts
+++ src/app/store.ts
@@ -1,10 +1,8 @@
 import { configureStore, ThunkAction, Action } from '@reduxjs/toolkit';
-import counterReducer from '../features/counter/counterSlice';
 import pokemonReducer from '../components/pokemonSlice'
 
 export const store = configureStore({
   reducer: {
-    counter: counterReducer,
     pokemon: pokemonReducer
   },
 });
